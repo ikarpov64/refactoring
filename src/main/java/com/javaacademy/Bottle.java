7845b08b42a4ba69713ebb5db6a38c3f32746879
Николай <kolyan7328@mail.ru>
2024-02-26 19:56:39 +0300

    Рефакторинг класса Bottle

--- src/main/java/com/javaacademy/Bottle.java
+++ src/main/java/com/javaacademy/Bottle.java
@@ -1,42 +1,29 @@
 package com.javaacademy;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 /**
  * Бутылка
  */
+@RequiredArgsConstructor
+@ToString(exclude = {"cityProducer"})
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Bottle {
-    private final double volume;
-    private double nestedVolume;
-    private final String cityProducer;
-
-    public Bottle(double volume, String cityProducer) {
-        this.volume = volume;
-        this.cityProducer = cityProducer;
-    }
+    @Getter
+    final double volume;
+    final String cityProducer;
+    @Setter
+    double nestedVolume;
 
     public void addLiquid(double liquidVolume) {
         if (liquidVolume > nestedVolume) {
-            throw  new RuntimeException("Объем добавляемой жидкости больше чем объем бутылки");
+            throw new RuntimeException("Объем добавляемой жидкости больше чем объем бутылки");
         }
         double newNestedVolume = nestedVolume + liquidVolume;
         if (newNestedVolume > volume) {
-            throw  new RuntimeException("Нет места в бутылки, все выливается!");
+            throw new RuntimeException("Нет места в бутылки, все выливается!");
         }
         setNestedVolume(newNestedVolume);
-    }
-
-    public void setNestedVolume(double nestedVolume) {
-        this.nestedVolume = nestedVolume;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-
-    @Override
-    public String toString() {
-        return "Bottle{" +
-                "volume=" + volume +
-                ", nestedVolume=" + nestedVolume +
-                '}';
     }
 }
