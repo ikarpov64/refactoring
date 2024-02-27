3e41b73fbaed99d608f8337912e887b3d6a76365
i.karpov <karpovia@volgir.com>
2024-02-27 19:17:38 +0400

    Refactoring of GarbageType and City classes

--- src/main/java/com/javaacademy/GarbageType.java
+++ src/main/java/com/javaacademy/GarbageType.java
@@ -1,17 +1,11 @@
 package com.javaacademy;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum GarbageType {
     PAPER("Бумага"), GLASS("Стекло"), MIXED("Смешанный мусор");
     private final String value;
-
-    GarbageType(String value) {
-        if (value == null) {
-            throw new NullPointerException("value is marked non-null, but is null");
-        }
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
 }
