84996a84d830139daa9112655fb833616497f38b
Николай <kolyan7328@mail.ru>
2024-02-27 22:28:36 +0300

    fix 2 UtilizationFactory

--- src/main/java/com/javaacademy/UtilizationFactory.java
+++ src/main/java/com/javaacademy/UtilizationFactory.java
@@ -1,11 +1,11 @@
 package com.javaacademy;
 
 import lombok.AccessLevel;
+import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.UtilityClass;
 
 import java.io.BufferedWriter;
-import java.io.IOException;
 
 /**
  * Фабрика по переработке мусора
@@ -15,22 +15,24 @@ import java.io.IOException;
 public class UtilizationFactory {
     double BOTTLE_SIZE = 500;
 
-    private Bottle refactorGlassGarbage(Garbage garbage) throws GarbageNotRefactorableException {
+    @SneakyThrows
+    private Bottle refactorGlassGarbage(Garbage garbage) {
         if (garbage.getGarbageType() != GarbageType.GLASS) {
             throw new GarbageNotRefactorableException("Мусор не состоит полностью из стекла!");
         }
         return new Bottle(BOTTLE_SIZE, garbage.getCityFrom());
     }
 
-    private Cartoon refactorPaperGarbage(Garbage garbage) throws GarbageNotRefactorableException {
+    @SneakyThrows
+    private Cartoon refactorPaperGarbage(Garbage garbage) {
         if (garbage.getGarbageType() != GarbageType.PAPER) {
             throw new GarbageNotRefactorableException("Мусор не состоит полностью из бумаги!");
         }
         return new Cartoon(garbage.getWeight() / 2);
     }
 
-    public void refactorGarbage(Garbage[] garbageArray, BufferedWriter journal)
-            throws GarbageNotRefactorableException, IOException {
+    @SneakyThrows
+    public void refactorGarbage(Garbage[] garbageArray, BufferedWriter journal) {
         for (Garbage garbage : garbageArray) {
             switch (garbage.getGarbageType()) {
                 case GLASS ->
