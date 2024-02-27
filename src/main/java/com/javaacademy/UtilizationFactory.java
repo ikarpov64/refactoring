7128da616dd29a9cf8adfc44ff16b4cd83f5296b
Николай <kolyan7328@mail.ru>
2024-02-27 22:13:03 +0300

    redacted UtilizationFactory

--- src/main/java/com/javaacademy/UtilizationFactory.java
+++ src/main/java/com/javaacademy/UtilizationFactory.java
@@ -1,37 +1,42 @@
 package com.javaacademy;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.UtilityClass;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 
 /**
  * Фабрика по переработке мусора
  */
+@UtilityClass
+@FieldDefaults(level = AccessLevel.PUBLIC, makeFinal = true)
 public class UtilizationFactory {
-    public static final double BOTTLE_SIZE = 500;
+    double BOTTLE_SIZE = 500;
 
-    private UtilizationFactory() {
-    }
-
-    private static Bottle refactorGlassGarbage(Garbage garbage) throws GarbageNotRefactorableException {
+    private Bottle refactorGlassGarbage(Garbage garbage) throws GarbageNotRefactorableException {
         if (garbage.getGarbageType() != GarbageType.GLASS) {
             throw new GarbageNotRefactorableException("Мусор не состоит полностью из стекла!");
         }
         return new Bottle(BOTTLE_SIZE, garbage.getCityFrom());
     }
 
-    private static Cartoon refactorPaperGarbage(Garbage garbage) throws GarbageNotRefactorableException {
+    private Cartoon refactorPaperGarbage(Garbage garbage) throws GarbageNotRefactorableException {
         if (garbage.getGarbageType() != GarbageType.PAPER) {
             throw new GarbageNotRefactorableException("Мусор не состоит полностью из бумаги!");
         }
         return new Cartoon(garbage.getWeight() / 2);
     }
 
-    public static void refactorGarbage(Garbage[] garbageArray, BufferedWriter journal)
+    public void refactorGarbage(Garbage[] garbageArray, BufferedWriter journal)
             throws GarbageNotRefactorableException, IOException {
         for (Garbage garbage : garbageArray) {
             switch (garbage.getGarbageType()) {
-                case GLASS -> journal.write(new JournalRecord(UtilizationFactory.refactorGlassGarbage(garbage)).toString());
-                case PAPER -> journal.write(new JournalRecord(UtilizationFactory.refactorPaperGarbage(garbage)).toString());
+                case GLASS ->
+                        journal.write(new JournalRecord(com.javaacademy.UtilizationFactory.refactorGlassGarbage(garbage)).toString());
+                case PAPER ->
+                        journal.write(new JournalRecord(com.javaacademy.UtilizationFactory.refactorPaperGarbage(garbage)).toString());
                 default -> journal.write(new JournalRecord(garbage.getWeight()).toString());
             }
         }
